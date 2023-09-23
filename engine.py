96e0d88d7bbcf98b199a823313935de15100c93e
Max Vu <mvu@berkeley.edu>
2023-09-22 19:27:05 -0700

    implemented TicTacToe board with main.py and engine.py scripts.

---
+++ engine.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+
+class GameState:
+    def __init__(self):
+        self.board = [
+            ["--", "--", "--"],
+            ["--", "--", "--"],
+            ["--", "--", "--"]
+        ]
+        self.human_to_move = True
+        self.move_log = []
---
+++ main.py
@@ -0,0 +1,56 @@
+import pygame
+import engine
+
+pygame.init()
+WIDTH = 636
+HEIGHT = 636
+DIMENSION = 3
+SQ_SIZE = HEIGHT // DIMENSION
+IMAGES = {}
+
+
+def main():
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+    screen.fill(pygame.Color("white"))
+    game_state = engine.GameState()
+    print(game_state.board)
+    load_images()
+        
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        draw_game_state(screen, game_state)
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
+
+def load_images():
+    symbols = ["x", "o"]
+    for symbol in symbols:
+        IMAGES[symbol] = pygame.transform.scale(pygame.image.load("images/" + symbol + ".png"), (SQ_SIZE, SQ_SIZE))
+
+def draw_game_state(screen, game_state):
+    draw_board(screen)
+    draw_symbols(screen, game_state.board)
+
+def draw_board(screen):
+    color = pygame.Color("black")
+    for row in range(DIMENSION):
+        for col in range(DIMENSION):
+            pygame.draw.rect(screen, color, pygame.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE), width = 5)
+
+def draw_symbols(screen, board):
+    for row in range(DIMENSION):
+        for col in range(DIMENSION):
+            symbol = board[row][col]
+            if symbol != "--":
+                screen.blit(IMAGES[symbol], pygame.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+main()
